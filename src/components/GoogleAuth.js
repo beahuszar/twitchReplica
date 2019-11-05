e51ffdd4f93c09d3f09050b45405d8c46c20d7c4
Huszar Beata <beata.huszar@adevinta.com>
2019-11-05 13:28:28 +0100

    User authentication with Google

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react'
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
 
   componentDidMount() {
+    // .load() => allows us to get a notification, when the loading process is complete
+    // by passing a callback function
+    //
+    // .init() => returns a promise (object), after the client library is successfully initalized
+    //
+    // .then() => when the client library is initalized, it will be automatically invoked 
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: `${process.env.REACT_APP_GOOGLE_CLIENT_ID}`,
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  }
+
+  onSignIn = () => {
+    this.auth.signIn();
+  }
+
+  onSignOut = () => {
+    this.auth.signOut();
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign out
+      </button >);
+    } else {
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign in with Google
+      </button>);
+    };
   }
 
   render() {
     return (
       <div>
-        Google Auth
+        {this.renderAuthButton()}
       </div>
     )
   }
