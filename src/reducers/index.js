61d7794043fe9e97bceda222f9417de535df67f0
Huszar Beata <beata.huszar@adevinta.com>
2019-11-05 16:50:17 +0100

    Wiring up Redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer // coming from the installed redux-form library
 });
