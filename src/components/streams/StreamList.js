54d7ae34a55477e073151f379cc31f97c1104fce
Huszar Beata <beata.huszar@adevinta.com>
2019-11-06 16:27:01 +0100

    Rendering admin buttons based on userId

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,10 +7,22 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderAdminButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdminButtons(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -34,7 +46,10 @@ class StreamList extends Component {
 const mapStateToProps = state => {
   // we have to map the store's objects back to array form
   // so that it is easier to iterate over in the component
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
