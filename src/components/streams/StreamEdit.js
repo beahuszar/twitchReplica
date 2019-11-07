c7b07ed057f6babd11cf59af87251c6088132239
Huszar Beata <beata.huszar@adevinta.com>
2019-11-07 10:09:08 +0100

    Fetching a stream by pathvariable

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreamById } from '../../actions';
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+// Props here includes details about the url
+// because this component is being rendered by the Route component
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStreamById(this.props.currentStreamId);
+  }
+
+  render() {
+    console.log(this.props);
+    return <div>StreamEdit</div>;
+  }
+}
+
+// ownProps are the same data as props in the function component
+const mapStateToProps = (state, ownProps) => {
+  const currentStreamId = ownProps.match.params.id;
+  // retrieve the stream object we need, by key
+  return {
+    stream: state.streams[currentStreamId],
+    currentStreamId
+  }
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStreamById })(StreamEdit);
